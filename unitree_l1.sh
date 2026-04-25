baae68ccbecd3df3a7192d00d792f65d5718f5bb
kazu-321 <kzs321kzs@gmail.com>
2026-04-25 12:45:00 +0900

    fix unilidar

--- unitree_l1.sh
+++ unitree_l1.sh
@@ -1,10 +1,12 @@
 cd ~
 git clone https://github.com/unitreerobotics/unilidar_sdk.git
-cd unilidar_sdk/unitree_lidar_sdk
+mv unilidar_sdk/unitree_lidar_sdk unitree_lidar_sdk
+cd unitree_lidar_sdk
 mkdir build
 cd build
 cmake .. && make -j2
 cd ~/unilidar_sdk/
-mv unitree_lidar_ros2 ~/robocon/src/unitree_lidar_ros2
+mv unitree_lidar_ros2/src/unitree_lidar_ros2 ~/robocon/src/unitree_lidar_ros2
 cd ~/robocon
 ./colcon_build.sh
+rm -rf ~/unilidar_sdk
